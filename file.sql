e5e935ec03e65035f17f519c48ae592bea94c2fb
zakarya hari <zakariahari42@gmail.com>
2025-12-02 11:57:59 +0100

    Creation de table Utilisateur.

--- file.sql
+++ file.sql
@@ -1,3 +1,6 @@
 Create database BlogCMS;
 
 use BlogCMS;
+
+create table utilisateur (username varchar(50) PRIMARY KEY unique , nom varchar(50) unique, prenom  varchar(50), email  varchar(100) unique, mod_de_pass  varchar(50), date_inscription date, role varchar(50) , constraint role_check check (role in ('user','admin')));
+
