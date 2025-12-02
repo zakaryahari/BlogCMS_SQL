559c23870de4b5be2a4c1648a960e01152b95ba6
zakarya hari <zakariahari42@gmail.com>
2025-12-02 12:37:29 +0100

    Ajouter des insertion de data aliatoire dans la table categorie.

--- file.sql
+++ file.sql
@@ -8,7 +8,7 @@ create table categorie (id_categorie int primary key auto_increment, libelle var
 
 create table article (id_article int primary key auto_increment, nom_article varchar(50), contenu text, date_creation date, date_modification date, id_categorie int, username varchar(50) , foreign key (id_categorie) references categorie (id_categorie) , foreign key (username) references utilisateur (username));
 
-create table commentaire (id_commentaire int primary key auto_increment, contenu_commentaire text, date_commentaire date,username varchar(50) null, id_article int , foreign key (id_article) references article (id_article) , foreign key (username) references utilisateur (username));
+create table commentaire (id_commentaire int primary key auto_increment, contenu_commentaire text, date_commentaire date, username varchar(50) null, id_article int , foreign key (id_article) references article (id_article) , foreign key (username) references utilisateur (username));
 
 
 
@@ -17,4 +17,14 @@ INSERT INTO utilisateur (username, nom, prenom, email, mod_de_pass, date_inscrip
 ('fatima_dev', 'Bennani', 'Fatima-Zahra', 'fatima.bennani@gmail.com', 'dev2024pass', '2024-02-15', 'user'),
 ('youssef_casa', 'Chraibi', 'Youssef', 'youssef.c@hotmail.fr', 'raja1949', '2024-03-20', 'user'),
 ('leila_travel', 'Tazi', 'Leila', 'leila.tazi@yahoo.com', 'travel2024', '2024-04-05', 'user');
+
+select * from utilisateur;
+
+INSERT INTO categorie (libelle) VALUES 
+('Technologie'),
+('Gastronomie'),
+('Sport & Botola'),
+('Tourisme Maroc');
+
+select * from categorie;
 
