252613ca0cc36f716c9e9af0b6b23f0d643bc8ce
zakarya hari <zakariahari42@gmail.com>
2025-12-03 00:19:01 +0100

    Ajouter les files de data do type csv.

--- file.sql
+++ file.sql
@@ -91,18 +91,6 @@ ALTER TABLE commentaire add email  varchar(100) unique;
 
 ALTER TABLE commentaire add status varchar(50) check (status in ('approved','spam'));
 
-
-CREATE TABLE commentaire (
-    id_commentaire INT PRIMARY KEY AUTO_INCREMENT, 
-    contenu_commentaire TEXT, 
-    date_commentaire DATE,
-    username VARCHAR(50) NULL, 
-    id_article INT, 
-    FOREIGN KEY (id_article) REFERENCES article (id_article), 
-    FOREIGN KEY (username) REFERENCES utilisateur (username)
-);
-
-
 ALTER TABLE commentaire ADD email VARCHAR(100) UNIQUE;
 ALTER TABLE commentaire ADD status VARCHAR(50) CHECK (status IN ('approved', 'spam', 'pending')); 
 
---
+++ querys.sql
@@ -0,0 +1,28 @@
+-- Q1:INSERT - Création d'un nouvel utilisateur
+
+ALTER table utilisateur add constraint role_check check (role in ('admin','author','éditeur'));
+
+SELECT * FROM utilisateur
+
+INSERT INTO utilisateur (username , email , mod_de_pass , date_inscription , role)
+VALUES ('jdupont','jean.dupont@email.com','$2y$10$...','2025-12-03','éditeur');
+
+-- Q2:SELECT - Récupération d'articles
+
+select nom_article , date_creation , status from article
+
+-- Q3:UPDATE - Modification de statut
+
+ALTER TABLE article drop constraint article_status_check;
+ALTER TABLE article add constraint article_status_check check (status in ('published','draft','archivé'));
+
+UPDATE article 
+set status = 'archivé'
+where date_creation < '2024-01-01';
+
+-- Q4:DELETE - Nettoyage des commentaires
+
+DELETE from commentaire 
+WHERE status = 'spam' AND date_commentaire < '2024-01-01';
+
+
