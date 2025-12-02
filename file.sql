1886515cc2bd942e32e5bf9961939530c7abd947
zakarya hari <zakariahari42@gmail.com>
2025-12-02 11:59:58 +0100

    Creation de table Commentaire.

--- file.sql
+++ file.sql
@@ -8,3 +8,4 @@ create table categorie (id_categorie int primary key auto_increment, libelle var
 
 create table article (id_article int primary key auto_increment, nom_article varchar(50), contenu varchar(500), date_création date, date_modification date, id_categorie int, username varchar(50) , foreign key (id_categorie) references categorie (id_categorie) , foreign key (username) references utilisateur (username));
 
+create table commentaire (id_commentaire int primary key auto_increment, contenu_commentaire varchar(50), date_commentaire date, username varchar(50), id_article int , foreign key (id_article) references article (id_article) , foreign key (username) references utilisateur (username));
