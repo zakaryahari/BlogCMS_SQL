b4f5d3ecf6c577f84264b2749d932ff4a140d303
zakarya hari <zakariahari42@gmail.com>
2025-12-02 11:58:43 +0100

    Creation de table Categorie.

--- file.sql
+++ file.sql
@@ -4,3 +4,5 @@ use BlogCMS;
 
 create table utilisateur (username varchar(50) PRIMARY KEY unique , nom varchar(50) unique, prenom  varchar(50), email  varchar(100) unique, mod_de_pass  varchar(50), date_inscription date, role varchar(50) , constraint role_check check (role in ('user','admin')));
 
+create table categorie (id_categorie int primary key auto_increment, libelle varchar(50));
+
